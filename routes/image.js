6b45d852fd643a4dc4c2801de9595c319ec5d68b
HuuDung <115625611+huydung7a5@users.noreply.github.com>
2023-09-23 10:26:09 +0700

    123
    123

--- routes/image.js
+++ routes/image.js
@@ -16,12 +16,12 @@ const storage = new CloudinaryStorage({
 const upload = multer({
      storage: storage,
 });
-router.post('/upload', upload.fields ([{ name: 'img', maxCount: 3 }]), async (req, res) => {
+router.post('/upload', upload.fields ([{ name: 'uri', maxCount: 3 }]), async (req, res) => {
     try {
         // tao model
-        const url1 = req.files['img'][0];
-        const url2 = req.files['img'][1];
-        const url3 = req.files['img'][2];
+        const url1 = req.files['uri'][0];
+        const url2 = req.files['uri'][1];
+        const url3 = req.files['uri'][2];
         const newInsert = { url1: url1.path, url2: url2.path, url3: url3.path };
         await modelimage.create(newInsert);
         res.json({ status: 1, message: ' thêm thành công' });
