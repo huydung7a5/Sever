7d4e4697d99dff11716b91657116c4bc9f4bc961
HuuDung <115625611+huydung7a5@users.noreply.github.com>
2023-10-05 14:02:21 +0700

    1
    1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Trang chủ',name:'đây là trang test 1' });
+    res.render('index', { title: 'Nguyễn Hữu Dũng',name:'đây là trang test 1' });
 });
 
 module.exports = router;
