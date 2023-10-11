2dba8f10e29953788643831ccbf04a61c25fb9a1
HuuDung <115625611+huydung7a5@users.noreply.github.com>
2023-10-11 14:15:28 +0700

    1
    1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var favicon  = require('serve-favicon');
+
 
 const mongoose = require('mongoose');
 require('./models/bida');
