255e46e110ceccf2b51b3269a23491e9a6a71887
HuuDung <115625611+huydung7a5@users.noreply.github.com>
2023-10-11 14:04:50 +0700

    updateLogo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var favicon  = require('serve-favicon');
 
 const mongoose = require('mongoose');
 require('./models/bida');
@@ -21,13 +21,16 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
+// render html
+app.engine('html', require('ejs').renderFile);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon (path.join(__dirname, '/public', 'favicon.ico')));
 //connect data
 mongoose.connect('mongodb+srv://admin:312337@cluster0.6imd0f4.mongodb.net/MyFPT?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -54,7 +57,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error.html');
 });
 
 module.exports = app;
